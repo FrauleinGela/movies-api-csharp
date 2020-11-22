126eedb1806ef783d7b66d6bf21e7bde6a164f31
Angela <angefraugela@gmail.com>
2020-11-23 00:25:18 +0100

    Add Auhorization and Authenticate

--- MoviesApi/MoviesApi/Startup.cs
+++ MoviesApi/MoviesApi/Startup.cs
@@ -1,11 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using MoviesApi.Models;
 using MoviesApi.Repository;
+using System.Security.Claims;
 
 namespace MoviesApi
 {
@@ -21,6 +24,21 @@ namespace MoviesApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddAuthentication(options => {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = Configuration["Auth0:Domain"];
+                    options.Audience = Configuration["Auth0:Audience"];
+                    // If the access token does not have a `sub` claim, `User.Identity.Name` will be `null`. Map it to a different claim by setting the NameClaimType below.
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = ClaimTypes.NameIdentifier
+                    };
+                });
             services.AddCors(options => options.AddPolicy("CorsPolicy",
                     builder => builder.WithOrigins("http://localhost:4200").AllowAnyMethod()
                     .AllowAnyHeader()));
@@ -32,8 +50,8 @@ namespace MoviesApi
             services.AddScoped<IMoviesRepository, MoviesRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors("CorsPolicy");
             if (env.IsDevelopment())
@@ -45,8 +63,9 @@ namespace MoviesApi
 
             app.UseRouting();
 
+           
+            app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
